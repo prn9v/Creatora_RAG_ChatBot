8f16c5db11a8e804ea0a68626714ad17acc942c4
Pranav Deshmukh <pranavdeshmukh5454@gmail.com>
2026-07-04 16:22:33 +0530

    changes in main to change host

--- main.py
+++ main.py
@@ -249,4 +249,4 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
